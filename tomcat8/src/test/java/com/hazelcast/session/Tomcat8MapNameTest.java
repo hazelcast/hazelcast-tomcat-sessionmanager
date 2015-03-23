2dade360e333fa5764e76fcacbd60cdd6cef66ae
Emrah Kocaman <emrahkocaman@gmail.com>
2015-03-23 17:18:24 +0200

    Fixes #161, skip jdk 1.6

--- tomcat8/src/test/java/com/hazelcast/session/Tomcat8MapNameTest.java
+++ tomcat8/src/test/java/com/hazelcast/session/Tomcat8MapNameTest.java
@@ -1,6 +1,11 @@
 package com.hazelcast.session;
 
+import org.junit.Rule;
+
 public class Tomcat8MapNameTest extends AbstractMapNameTest{
+
+    @Rule
+    public Java6ExcludeRule java6ExcludeRule = new Java6ExcludeRule();
 
     @Override
     protected WebContainerConfigurator<?> getWebContainerConfigurator() {
