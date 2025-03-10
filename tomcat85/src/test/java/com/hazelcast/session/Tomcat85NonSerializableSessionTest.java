ec3104ec566d0d4fb36cf04b222a074b05251014
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2025-03-10 13:25:58 +0100

    Remove Tomcat 8.5

--- tomcat85/src/test/java/com/hazelcast/session/Tomcat85NonSerializableSessionTest.java
+++
@@ -1,8 +0,0 @@
-package com.hazelcast.session;
-
-public class Tomcat85NonSerializableSessionTest extends AbstractNonSerializableSessionTest {
-    @Override
-    protected WebContainerConfigurator<?> getWebContainerConfigurator() {
-        return new Tomcat85Configurator();
-    }
-}
