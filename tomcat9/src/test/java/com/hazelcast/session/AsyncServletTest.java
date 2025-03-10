b73303c99187d03bd0c1583f826dc73ca1cf9fdc
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2025-03-10 13:26:14 +0100

    Remove Tomcat 9

--- tomcat9/src/test/java/com/hazelcast/session/AsyncServletTest.java
+++
@@ -1,8 +0,0 @@
-package com.hazelcast.session;
-
-public class AsyncServletTest extends AbstractAsyncServletTest {
-    @Override
-    protected WebContainerConfigurator<?> getAsyncWebContainerConfigurator() {
-        return new Tomcat9AsyncConfigurator(temporaryFolder.getRoot().getAbsolutePath());
-    }
-}
--- tomcat9/src/test/java/com/hazelcast/session/Tomcat9NonSerializableSessionTest.java
+++
@@ -1,9 +0,0 @@
-package com.hazelcast.session;
-
-public class Tomcat9NonSerializableSessionTest
-        extends AbstractNonSerializableSessionTest {
-    @Override
-    protected WebContainerConfigurator<?> getWebContainerConfigurator() {
-        return new Tomcat9Configurator();
-    }
-}
